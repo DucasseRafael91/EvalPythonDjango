02a384d81618603525d7a1b18f83bb62409f3b38
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-03-09 11:20:59 +0100

    Ajout de la classe CompetenceAdmin et CategorieAdmin

--- competences/admin.py
+++ competences/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from competences.models import Categorie, Competence, UtilisateurCompetence, Slot
 
-admin.site.register(Categorie)
-admin.site.register(Competence)
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+class CompetenceAdmin(admin.ModelAdmin):
+    list_display = ["name", "categorie"]
+
+
+admin.site.register(Categorie,CategorieAdmin)
+admin.site.register(Competence, CompetenceAdmin)
 admin.site.register(UtilisateurCompetence)
 admin.site.register(Slot)
