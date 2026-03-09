cdd6771c00c6ebd872771f10f0a8ee95723a55f1
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-03-09 10:49:30 +0100

    Création du model Competence

--- competences/models.py
+++ competences/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 class Categorie(models.Model):
     name = models.CharField(max_length=50)
+
+class Competence(models.Model):
+    name = models.CharField(max_length=50)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
