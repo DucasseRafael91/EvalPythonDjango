bdd5cde6e940dc06372a1ce97a9ccbc75288bf23
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-03-09 13:42:41 +0100

    Ajout des competences dans la sidebar

--- competences/views.py
+++ competences/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from competences.models import Slot
+from competences.models import Slot, Competence
 
 
 def index(request):
-    slots_list = Slot.objects.all()
-    context = {"slots_list": slots_list}
+    slots_list = Slot.objects.filter(helper_user__isnull=False).order_by("-date")
+    competences_list = Competence.objects.all()
+    context = {"slots_list": slots_list, "competences_list": competences_list}
     return render(request, "competences/index.html", context)
