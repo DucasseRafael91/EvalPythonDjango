3c4d09a203d623775c37c86f78712ca40ca63d30
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-03-09 11:20:26 +0100

    Ajout d'un str pour la classe Categorie

--- competences/models.py
+++ competences/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Categorie(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Competence(models.Model):
     name = models.CharField(max_length=50)
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
