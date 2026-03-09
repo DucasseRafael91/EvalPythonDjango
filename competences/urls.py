57845eda787f75c5d5e3eb7a2ec54babcd83b085
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-03-09 15:45:55 +0100

    Ajout des routes pour accéder au template search

--- competences/urls.py
+++ competences/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('skills/', views.skills, name='skills'),
     path('add/', views.add_slot, name='add'),
+    path('search/', views.search, name='search'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
