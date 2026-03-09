a08a74a3d6b2d65a730b51006d1d954a77e46730
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-03-09 13:14:18 +0100

    Test de l'affichage de données dans index.html

--- competences/views.py
+++ competences/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from competences.models import Slot
+
 
 def index(request):
-    return render(request, "competences/index.html")
+    slots_list = Slot.objects.all()
+    context = {"slots_list": slots_list}
+    return render(request, "competences/index.html", context)
