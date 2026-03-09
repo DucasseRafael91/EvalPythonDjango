11469a86b912b39b01d5ceffff6d13227fff2b2e
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-03-09 10:53:58 +0100

    Renommage de la classe UtilisateurCompetence

--- competences/models.py
+++ competences/models.py
@@ -7,7 +7,7 @@ class Competence(models.Model):
     name = models.CharField(max_length=50)
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
 
-class Utilisateur_Competence(models.Model):
+class UtilisateurCompetence(models.Model):
     utilisateur = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     competence = models.ForeignKey(Competence, on_delete=models.CASCADE)
 
