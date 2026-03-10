fbb445f1260ec1a8cc96059e7fab9e1f064a30f6
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-03-10 11:24:58 +0100

    Ajout du typage statique dans models.py

--- competences/models.py
+++ competences/models.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User  # type: ignore
 from django.db import models  # type: ignore
 
+
 class Categorie(models.Model):
     name: models.CharField = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
 
 class Competence(models.Model):
     name: models.CharField = models.CharField(max_length=50)
@@ -14,14 +16,17 @@ class Competence(models.Model):
     def __str__(self):
         return self.name
 
+
 class UserCompetence(models.Model):
-    user: models.ForeignKey  = models.ForeignKey(User, on_delete=models.CASCADE)
+    user: models.ForeignKey = models.ForeignKey(User, on_delete=models.CASCADE)
     competence: models.ForeignKey = models.ForeignKey(Competence, on_delete=models.CASCADE)
+
 
 class Slot(models.Model):
     creator_user: models.ForeignKey = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,
-                                     related_name='slots_created')
-    helper_user: models.ForeignKey = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='slots_helped')
+                                                        related_name='slots_created')
+    helper_user: models.ForeignKey = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
+                                                       related_name='slots_helped')
     competence: models.ForeignKey = models.ForeignKey(Competence, on_delete=models.CASCADE)
     activity: models.CharField = models.CharField(max_length=200)
     date: models.DateField = models.DateField()
