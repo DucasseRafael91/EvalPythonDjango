c4a99e4bfbaeb1ff2831ef8812da97d5218c3af3
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-03-10 11:23:29 +0100

    Ajout du typage statique dans models.py

--- competences/models.py
+++ competences/models.py
@@ -2,29 +2,29 @@ from django.contrib.auth.models import User  # type: ignore
 from django.db import models  # type: ignore
 
 class Categorie(models.Model):
-    name = models.CharField(max_length=50)
+    name: models.CharField = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 class Competence(models.Model):
-    name = models.CharField(max_length=50)
-    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    name: models.CharField = models.CharField(max_length=50)
+    categorie: models.ForeignKey = models.ForeignKey(Categorie, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class UserCompetence(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    competence = models.ForeignKey(Competence, on_delete=models.CASCADE)
+    user: models.ForeignKey  = models.ForeignKey(User, on_delete=models.CASCADE)
+    competence: models.ForeignKey = models.ForeignKey(Competence, on_delete=models.CASCADE)
 
 class Slot(models.Model):
-    creator_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,
+    creator_user: models.ForeignKey = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,
                                      related_name='slots_created')
-    helper_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='slots_helped')
-    competence = models.ForeignKey(Competence, on_delete=models.CASCADE)
-    activity = models.CharField(max_length=200)
-    date = models.DateField()
+    helper_user: models.ForeignKey = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='slots_helped')
+    competence: models.ForeignKey = models.ForeignKey(Competence, on_delete=models.CASCADE)
+    activity: models.CharField = models.CharField(max_length=200)
+    date: models.DateField = models.DateField()
 
     def __str__(self):
         return f"{self.activity} - {self.competence.name} - {self.date}"
